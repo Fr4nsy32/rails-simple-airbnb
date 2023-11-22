722b448479260bf1cc58bf1a89fb04581dd9dc2e
Andi Sina <andy.sina32@gmail.com>
2023-11-22 16:48:07 +0100

    Started the index

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,14 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+require 'faker'
+
+5.times do
+Flat.create!(
+  name: "#{Faker::Verb.base} in #{Faker::Address.city} near #{Faker::Address.community}",
+  address: Faker::Address.full_address,
+  description: Faker::Lorem.sentence,
+  price_x_night: Faker::Number.between(from: 30, to: 180),
+  number_of_guests: Faker::Number.between(from: 1, to: 5)
+)
+end
